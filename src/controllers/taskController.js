b614969ee139732fbb064ebfe89161ccb9ca93bf
Jake <jakefabz@gmail.com>
2026-04-08 23:32:40 -0400

    Updated getTasks logic

--- src/controllers/taskController.js
+++ src/controllers/taskController.js
@@ -3,8 +3,8 @@ import { getTasksByCompletionStatus } from '../services/taskService.js';
 
 export async function getTasks(req, res, next) { 
   const {completed} = req.query; //Need to make this optional for case where no query parameter is provided 
-  if (completed) { 
-    const isCompleted = completed.toLowerCase() === 'true'; 
+  if (completed && completed.toLowerCase() === 'true') { 
+    const isCompleted = completed.toLowerCase() === 'true';
     const filteredTasks = await getTasksByCompletionStatus(isCompleted); 
     res.json(filteredTasks);
   } else  if (completed && completed.toLowerCase() === 'false') { 
