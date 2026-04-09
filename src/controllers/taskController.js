5025b71a761a009992f87e6e458c406450c9c977
Jake <jakefabz@gmail.com>
2026-04-08 23:37:43 -0400

    Fix: Fixed Logic error in taskController

--- src/controllers/taskController.js
+++ src/controllers/taskController.js
@@ -8,12 +8,12 @@ export async function getTasks(req, res, next) {
     const filteredTasks = await getTasksByCompletionStatus(isCompleted); 
     res.json(filteredTasks);
   } else  if (completed && completed.toLowerCase() === 'false') { 
-    const notCompleted = completed.toLowerCase() === 'false'; 
+    const notCompleted = completed.toLowerCase(); 
     const incompletedFilteredTasks = await getTasksByCompletionStatus(notCompleted); 
     res.json(incompletedFilteredTasks);
   } 
   else if (completed && completed.toLowerCase() !== 'true' && completed.toLowerCase() !== 'false') { 
-    return res.status(400).json({ error: 'Invalid value for completed parameter. Use true or false.' });
+    res.status(400).json({ error: 'Invalid value for completed parameter. Use true or false.' });
   }
   const tasks = await taskService.getAllTasks();
   res.json(tasks);
